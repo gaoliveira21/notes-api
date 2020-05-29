2f9cf8183ac9db2b902d199c8980bcdd9ab3fc9f
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-29 09:39:52 -0300

    user validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,23 @@
+import * as yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(request, response) {
-    const user = await User.create(request.body);
-    return response.json(user);
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().min(6).max(25).required(),
+    });
+
+    await schema.validate(request.body).catch((err) =>
+      response.status(400).json({
+        success: false,
+        error: err.name,
+        details: err.errors,
+      })
+    );
+
+    return response.json();
   }
 }
 
